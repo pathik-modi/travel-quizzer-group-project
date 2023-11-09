33685f35515c54d038763599d6de2ac0a31f9550
Pathik Modi <modipathik95@gmail.com>
2023-11-10 12:44:22 +1300

    working frontend tidy homepage

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -18,25 +18,7 @@ function HomePage() {
             <Link to="/frogs/xavier">Quiz Page</Link>
           </button>
         </div>
-
-        {/* <div>
-          <h2>Countires</h2>
-          <p>Explore Route</p>
-          <Link to="/frogs/xavier">Countries</Link>
-        </div> */}
       </div>
-      {/* 
-      {count}
-      <h1>{greeting}</h1>
-      {isError && (
-        <p style={{ color: 'red' }}>
-          There was an error retrieving the greeting.
-        </p>
-      )}
-      <button onClick={() => setCount(count + 1)}>Click</button>
-      <p>
-        <Link to="/frogs/xavier">Is there a frog named Kevin?</Link>
-      </p> */}
     </>
   )
 }
