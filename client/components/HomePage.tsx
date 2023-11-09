5cd3449b35d136a6515d51f90f4541cd48a208e7
Pathik Modi <modipathik95@gmail.com>
2023-11-10 12:43:45 +1300

    working frontend

--- client/components/HomePage.tsx
+++ client/components/HomePage.tsx
@@ -1,25 +1,11 @@
 import { useState, useEffect } from 'react'
-import { getGreeting } from '../apiClient.ts'
+import { getCitiesData } from '../apiClient.ts'
 import { Link } from 'react-router-dom'
 
 function HomePage() {
   const [greeting, setGreeting] = useState('')
   const [count, setCount] = useState(0)
   const [isError, setIsError] = useState(false)
-
-  useEffect(() => {
-    async function updateGreeting() {
-      try {
-        const greeting = await getGreeting()
-        setGreeting(greeting)
-        setIsError(false)
-      } catch (err) {
-        setIsError(true)
-      }
-    }
-
-    updateGreeting()
-  }, [count])
 
   return (
     <>
