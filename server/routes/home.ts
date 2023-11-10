a427b64a2fd31db774bc7d3ee41a98a13a98c475
Dan <143304904+danial-shahali@users.noreply.github.com>
2023-11-10 15:56:21 +1300

    Add logic to apiclient

--- server/routes/home.ts
+++ server/routes/home.ts
@@ -1,14 +1,13 @@
 import express from 'express'
 import { getAllCitiesAndAttributes, getAllCitiesData } from '../db/db'
-import { addUserInput } from '../../client/apiClient'
 
 const router = express.Router()
 
 // const attributes = {
-//   atr1: 'Art',
-//   atr2: 'Fine Dining',
-//   atr3: 'Shopping',
-//   atr4: 'Culture',
+//   atrOne: 'Art',
+//   atrTwo: 'Fine Dining',
+//   atrThree: 'Shopping',
+//   atrFour: 'Culture',
 // }
 
 router.get('/', async (req, res) => {
@@ -21,67 +20,67 @@ router.get('/', async (req, res) => {
   }
 })
 
-router.post('/quiz', async (req, res) => {
-  try {
-    const attributes = req.body
+// router.post('/quiz', async (req, res) => {
+//   try {
+//     const attributes = req.body
 
-    res.sendStatus(201)
-  } catch (error) {
-    if (error instanceof Error) {
-      console.error(error)
-      res.status(500).json({ error: 'woopsie server error' })
-    }
-  }
-})
+//     res.sendStatus(201)
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       console.error(error)
+//       res.status(500).json({ error: 'woopsie server error' })
+//     }
+//   }
+// })
 
 router.get('/quiz', async (req, res) => {
   try {
     const getAll = await getAllCitiesAndAttributes()
 
-    const atr1array = getAll.filter((object) => {
-      return object.attribute === attributes.atrOne
-    })
-    const atr2array = getAll.filter((object) => {
-      return object.attribute === attributes.atrTwo
-    })
-    const atr3array = getAll.filter((object) => {
-      return object.attribute === attributes.atrThree
-    })
-    const atr4array = getAll.filter((object) => {
-      return object.attribute === attributes.atrFour
-    })
+    // const atr1array = getAll.filter((object) => {
+    //   return object.attribute === attributes.atrOne
+    // })
+    // const atr2array = getAll.filter((object) => {
+    //   return object.attribute === attributes.atrTwo
+    // })
+    // const atr3array = getAll.filter((object) => {
+    //   return object.attribute === attributes.atrThree
+    // })
+    // const atr4array = getAll.filter((object) => {
+    //   return object.attribute === attributes.atrFour
+    // })
 
-    const getAllCitiesAndAttributesArray = atr1array.concat(
-      atr2array,
-      atr3array,
-      atr4array
-    )
-    const citiesArray = getAllCitiesAndAttributesArray.map((ob) => ob.city)
+    // const getAllCitiesAndAttributesArray = atr1array.concat(
+    //   atr2array,
+    //   atr3array,
+    //   atr4array
+    // )
+    // const citiesArray = getAllCitiesAndAttributesArray.map((ob) => ob.city)
 
-    const cityCounts = citiesArray.reduce(
-      (count, item) => ((count[item] = count[item] + 1 || 1), count),
-      {}
-    )
+    // const cityCounts = citiesArray.reduce(
+    //   (count, item) => ((count[item] = count[item] + 1 || 1), count),
+    //   {}
+    // )
 
-    console.log(cityCounts)
+    // console.log(cityCounts)
 
-    const cityCountsArray = Object.entries(cityCounts)
+    // const cityCountsArray = Object.entries(cityCounts)
 
-    const sortedArray = cityCountsArray
-      .map((obj) => {
-        return { name: obj[0], count: obj[1] }
-      })
-      .sort((a, b) => a.count - b.count)
-      .reverse()
+    // const sortedArray = cityCountsArray
+    //   .map((obj) => {
+    //     return { name: obj[0], count: obj[1] }
+    //   })
+    //   .sort((a, b) => a.count - b.count)
+    //   .reverse()
 
-    console.log(sortedArray)
+    // console.log(sortedArray)
 
-    sortedArray.splice(3)
+    // sortedArray.splice(3)
 
-    const topThreeCities = sortedArray.map((obj) => obj.name)
+    // const topThreeCities = sortedArray.map((obj) => obj.name)
 
     // console.log(attributes.atr1)
-    res.json(topThreeCities)
+    res.json(getAll)
 
     // res.sendStatus(200)
   } catch (error) {
