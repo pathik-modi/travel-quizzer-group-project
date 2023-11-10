0c08a6f5437e0bfa1596f4f5798b516e84a8c16a
Dan <143304904+danial-shahali@users.noreply.github.com>
2023-11-10 14:22:02 +1300

    Logic sorted for top three

--- server/db/db.ts
+++ server/db/db.ts
@@ -8,5 +8,5 @@ export function getAllCitiesAndAttributes() {
   return db('cities')
     .join('attributesOfCities', 'city_id', 'cities.id')
     .join('attributes', 'attributes.id', 'attribute_id')
-    .select('city_id as cityId', 'attribute_id as attributeId')
+    .select('city', 'attribute')
 }
