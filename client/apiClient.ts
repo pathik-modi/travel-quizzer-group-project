ffb77902675bc83017b7ee6a97553443f1ca36cc
Pathik Modi <modipathik95@gmail.com>
2023-11-10 14:26:42 +1300

    tidy data and add city routes

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,11 +1,10 @@
 import { log } from 'console'
 import request from 'superagent'
 
-const citiesURL = 'api/v1'
+const citiesURL = '/api/v1'
 
 export async function getCitiesData() {
   const res = await request.get(`${citiesURL}`)
-  console.log(res.body)
 
   return res.body
 }
--- client/components/CitiesPage.tsx
+++ client/components/CitiesPage.tsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getCitiesData } from '../apiClient'
+import { log } from 'node:console'
 
 export default function CitiesPage() {
-  const [loadcitiesNames, setLoadCities] = useState()
+  const [cities, setCities] = useState([])
+  useEffect(() => {
+    loadCities()
+  }, [])
 
   async function loadCities() {
     const body = await getCitiesData()
-    setLoadCities(body)
+    setCities(body)
   }
-  loadCities()
+
+  console.log(cities)
+
   return (
     <>
       <h2>Cities Page here</h2>
-      {loadcitiesNames}
+      <div></div>
     </>
   )
 }
