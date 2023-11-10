9f1bc055b0ba416fcee0e514799a06bc49219a70
Xavier <143090232+xavier-kirkpatrick@users.noreply.github.com>
2023-11-10 13:37:12 +1300

    add cities page

--- client/components/CitiesPage.tsx
+++ client/components/CitiesPage.tsx
@@ -1,3 +1,18 @@
+import { useState } from 'react'
+import { getCitiesData } from '../apiClient'
+
 export default function CitiesPage() {
-  return <h2>Cities Page here</h2>
+  const [loadcitiesNames, setLoadCities] = useState()
+
+  async function loadCities() {
+    const body = await getCitiesData()
+    setLoadCities(body)
+  }
+  loadCities()
+  return (
+    <>
+      <h2>Cities Page here</h2>
+      {loadcitiesNames}
+    </>
+  )
 }
