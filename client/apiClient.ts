9558769a95753a4b0c7b8029e46130dfde01412d
Dan <143304904+danial-shahali@users.noreply.github.com>
2023-11-10 15:01:24 +1300

    updated something

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,32 +1,11 @@
-import { log } from 'console'
 import request from 'superagent'
 
 const citiesURL = '/api/v1'
 
-const attributes = {
-  atr1: 'Art',
-  atr2: 'Fine Dining',
-  atr3: 'Shopping',
-}
-
 export async function getCitiesData() {
+  const res = await request.get(`${citiesURL}`)
 
-  const res = await request.get(citiesURL)
-  console.log(res.body)
-  // return res.body
+  return res.body
 }
 
-export async function getAllCitiesAndAttributes() {
-  const res = await request.get(`${citiesURL}/quiz`)
-  const cityAndAttributeArray = res.body
-
-  const atr1array = cityAndAttributeArray.filter(
-    (object) => object.attribute == attributes.atr1
-  )
-
-  console.log(attributes.atr1)
-  console.log(res)
-  return atr1array
-
-
-}
+getCitiesData()
